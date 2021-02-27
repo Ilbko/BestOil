21d381c07e0740e870c77a4c7b4b73d3e478d2b4
Sepe_4bld@student.itstep.org <Sepe_4bld@student.itstep.org>
2021-02-27 14:36:55 +0200

    FirstPanel Elements

--- BestOil/Form1.Designer.cs
+++ BestOil/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Windows.Forms;
+
 namespace BestOil
 {
     partial class Form1
@@ -35,6 +37,35 @@ namespace BestOil
             this.Text = "BestOil";
         }
 
+        #endregion
+
+        /*Принцип наименования элементов:
+          - Все элементы имеют в своём названии свой тип.
+          - Элементы группы, принадлежащие какой-либо главной панели, в начале
+            своего названия имеют имя панели.
+          - Имя элементов указывает на их функцию/роль.
+          - Если элемент, а именно Label, предназначен для неизменимого в роли программмы
+            вывода текста (для описания действия какого-либо элемента), то в своём названии
+            он имеет приставку "text".*/
+        #region Первая группа элементов - "Автозаправка"
+        Panel panel_station;
+        ComboBox station_combo_fuel;
+            Label station_label_fuel_text;
+        Label station_label_fuelprice;
+            Label station_label_fuelprice_text;
+
+        Panel station_panel_radio;
+        RadioButton station_radio_amount;
+        RadioButton station_radio_sum;
+
+        TextBox station_textbox_amount;
+            Label station_label_amount_text;
+        TextBox station_textbox_sum;
+            Label station_label_sum_text;
+
+        Panel station_panel_payment;
+        Label station_label_payment;
+            Label station_label_payment_text;
         #endregion
     }
 }
